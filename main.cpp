7633a157cb955703a182a2e0ef986fdebf0247f9
JhonGarc <Jhon61631@gmail.com>
2022-03-13 01:29:46 -0500

    class Persona
    agregue la clase persona con getter y setters

--- main.cpp
+++ main.cpp
@@ -1,16 +1,60 @@
 #include <iostream>
 
+class Persona
+{
+private:
+    string nombre, edad, fechaNacimiento, direccion, telefono, tipoDocumento, documento;
+
+public:
+    Persona()
+    {
+        nombre = edad = fechaNacimiento = direccion = telefono = tipoDocumento = documento = "";
+    }
+    Persona(string nombre, string edad, string fechaNacimiento, string direccion, string telefono, string tipoDocumento, string documento)
+    {
+        this->nombre = nombre;
+        this->edad = edad;
+        this->fechaNacimiento = fechaNacimiento;
+        this->direccion = direccion;
+        this->telefono = telefono;
+        this->tipoDocumento = tipoDocumento;
+        this->documento = documento;
+    }
+
+    void setNombre(string nombre)
+    {
+        this->nombre = nombre;
+    }
+    void setEdad(string edad)
+    {
+        this->edad = edad;
+    }
+    void setFechaNacimiento(string setFechaNacimiento)
+    {
+        this->fechaNacimiento = fechaNacimiento;
+    }
+    void setDireccion(string direccion)
+    {
+        this->direccion = direccion;
+    }
+    void setTelefono(string telefono)
+    {
+        this->telefono = telefono;
+    }
+    void setTipoDocumento(string tipoDocumento)
+    {
+        this->tipoDocumento = tipoDocumento;
+    }
+    void setDocumento(string documento)
+    {
+        this->documento = documento;
+    }
+};
+
 using namespace std;
 
-int main(){
-    cout << "Hello word" << endl;
-    cout << "Bye word" << endl;
-    cout << "Hola";
-
-    cout << "Bienvenida Maibeth";
-    cout << "gracias uwu";
-
-    cout << "chupe la que cuelgue"<< endl;
+int main()
+{
 
     return 0;
 }
